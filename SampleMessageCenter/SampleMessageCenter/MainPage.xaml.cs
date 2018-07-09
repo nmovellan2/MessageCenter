87c85a4f1452db0fcba63bd88f4a70b96542a95d
Nacho Movellan <nmovellan2@hotmail.com>
2018-07-09 16:39:41 +0200

    Agregar archivos de proyecto.

---
+++ SampleMessageCenter/SampleMessageCenter/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using SampleMessageCenter.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SampleMessageCenter
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+
+            //subscribirse a un evento
+            MessagingCenter.Subscribe<MainViewModel>(this, "LimitAge", (vm) => {
+                DisplayAlert("Error", "Age avereached", "Cancel");
+            });
+
+        }
+    }
+}
---
+++ SampleMessageCenter/SampleMessageCenter/ViewModel/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SampleMessageCenter.ViewModel
+{
+    //para notificar los eventos a la vista
+    public class MainViewModel: BindableObject 
+    {
+        private int _age;
+        public int Age {
+            get => _age;
+            set
+            {
+                _age = value;
+
+                OnPropertyChanged(); //metodo heredado de BindableObject
+                if(_age > 90)
+                {
+                    MessagingCenter.Send(this, "LimitAge");
+                }
+
+            }
+        }
+    }
+}
